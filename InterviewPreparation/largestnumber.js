0925f15706171d30857b029466364aec5add6372
M Vishnu vardhan <vishnuvardhan7823@gmail.com>
2025-02-12 22:05:45 +0530

    largestnum problem

--- InterviewPreparation/largestnumber.js
+++ InterviewPreparation/largestnumber.js
@@ -9,4 +9,20 @@ function largestnumber(arr){
 
 }
 
-console.log(largestnumber([1,2,67,89,24,35]))
+console.log(largestnumber([1,2,67,89,24,35]))   
+
+
+//using loop method
+
+function largest(arr){
+    let largestnumber = arr[0];
+
+    for(let i=0;i<=arr.length;i++){
+        if(arr[i]>largestnumber){
+            largestnumber = arr[i];
+        }
+    }
+    return largestnumber;
+}
+
+console.log(largest([1,5,4,3,8,2]))
